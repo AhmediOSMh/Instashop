64fff456ef8c98b0601ab1eee1d97e06611c70cd
Ahmed Mh <ahmedmh@MacBook-Pro-de-Ahmed.local>
2018-10-06 13:07:29 +0100

    Initial Commit

---
+++ InstaShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaShop
+//
+//  Created by Ahmed Mh on 06/10/2018.
+//  Copyright © 2018 Ahmed Mh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
