daaefd99ce5443905d3da4109b27e2ad6df86a65
Ahmed Mh <ahmedmh@MacBook-Pro-de-Ahmed.local>
2018-10-10 22:27:55 +0100

    add pods + localizable file + icons + signUpScreen

---
+++ InstaShop/Authentication/SignUp/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  InstaShop
+//
+//  Created by Ahmed Mh on 10/10/2018.
+//  Copyright © 2018 Ahmed Mh. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-InstaShop/Pods-InstaShop-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InstaShop : NSObject
+@end
+@implementation PodsDummy_Pods_InstaShop
+@end
