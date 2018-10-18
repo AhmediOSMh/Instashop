ec3e1e95e609888b63829278b3437b4dd8ae31ec
Ahmed Mh <ahmedmh@MacBook-Pro-de-Ahmed.local>
2018-10-18 20:58:40 +0100

    signup

---
+++ InstaShop/Component/CustomRadioButton.swift
@@ -0,0 +1,42 @@
+//
+//  CustomRadioButton.swift
+//  InstaShop
+//
+//  Created by Ahmed Mh on 18/10/2018.
+//  Copyright © 2018 Ahmed Mh. All rights reserved.
+//
+
+import UIKit
+
+class RadioButton: UIButton {
+    var alternateButton:Array<RadioButton>?
+    
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 5
+        self.layer.borderWidth = 2.0
+        self.layer.masksToBounds = true
+    }
+    
+    func unselectAlternateButtons(){
+        if alternateButton != nil {
+            self.isSelected = true
+            
+            for aButton:RadioButton in alternateButton! {
+                aButton.isSelected = false
+            }
+        }else{
+            toggleButton()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        unselectAlternateButtons()
+        super.touchesBegan(touches, with: event)
+    }
+    
+    func toggleButton(){
+        self.isSelected = !isSelected
+    }
+
+}
+
