1ea4f2616c4348dce2f3c63f17ac617ae0fc3ce7
Ahmed Mh <ahmed.mahouchi@machinestalk.com>
2018-11-01 16:42:17 +0100

    add map view

--- InstaShop/GPS/GPSViewController.swift
+++ InstaShop/GPS/GPSViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class GPSViewController: BaseController {
 
     @IBOutlet weak var address: TextField!
     @IBOutlet weak var gpsLbl: Label!
     @IBOutlet weak var GpsBtn: Button!
-    @IBOutlet weak var mapView: UIView!
+    @IBOutlet weak var Viewmap: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadView()
         // Do any additional setup after loading the view.
     }
-
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
